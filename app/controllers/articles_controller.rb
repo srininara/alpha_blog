5aa6eae5300c4187094534ff65763f60f007bf3d
Srinivas Narayanan <srininara@gmail.com>
2018-09-27 09:27:07 +0530

    Partials support. Delete Support. Link from home

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,6 +12,14 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    
+    flash[:notice] = "Article deleted!"
+    redirect_to articles_path
+  end
+
   def create
     @article = Article.new(article_params)
     if @article.save
